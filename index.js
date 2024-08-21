05f0812c6e5c857fd138537630b0fd696387eb5a
Lamiiii <lamiyekazimov37@gmail.com>
2024-08-21 13:48:47 +0400

    js-first

---
+++ index.js
@@ -0,0 +1,38 @@
+let name="lamiye";
+console.log(name);
+console.log(typeof name);
+
+
+let number1=10;
+let number2=30;
+let number=40;
+let number4=30;
+console.log(number1+number2);
+console.log(Number(number1)+Number(number2));
+
+ let surname="Kazimova";
+let isim;
+console.log(name+" "+surname);
+ 
+
+console.log(number1.toString()+number2.toString());
+
+ console.log(typeof null);
+
+ console.log(number1==number2);
+ console.log(typeof true);
+ console.log(true+number2);
+ console.log(number2>number4);
+ console.log(number/number2);
+console.log(number%number2);
+ console.log(Math.round(number/number2));
+ console.log(isim);
+ console.log(typeof isim);
+ let text=`${name} ${surname}`;
+ console.log(text);
+
+
+
+
+
+
